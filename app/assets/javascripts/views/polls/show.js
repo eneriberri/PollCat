39016dd52c9804abb3cb07ac021c3b0d63675fe1
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-11-06 17:39:45 -0800

    chart shows on individual poll pages

--- app/assets/javascripts/views/polls/show.js
+++ app/assets/javascripts/views/polls/show.js
@@ -2,7 +2,6 @@ PollCatApp.Views.PollShow = Backbone.View.extend({
   template: JST['polls/show'],
 
   render: function() {
-    console.log(this.model.attributes);
     var renderedHTML = this.template({ poll: this.model });
     this.$el.html(renderedHTML);
     return this;
