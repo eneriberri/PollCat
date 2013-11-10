dbde36ca93d687505aee4f49f27a19b99e3c7def
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-11-10 03:14:37 -0800

    default piechart when new poll

--- app/assets/javascripts/views/polls/show.js
+++ app/assets/javascripts/views/polls/show.js
@@ -19,33 +19,26 @@ PollCatApp.Views.PollShow = Backbone.View.extend({
 
   renderChart: function() {
     var votes = this.collection.where({ poll_id: parseInt(this.id) });
-    console.log(this.voteFreq);
+    if(_.keys(this.voteFreq).length === 0) {
+      var ctxInitial = this.$el.find('#myChart').get(0).getContext("2d");
+      new Chart(ctxInitial).Pie([{ value: 1, color: "#000000" }]);
+    }
+    else {
+      var pieData = [];
+      var colors = ["#F38630", "#E0E4CC", "#69D2E7"];
 
-    // var vote_freq = {};
-    // for(var i = 0; i < votes.length; i++) {
-    //   var vote = parseInt(votes[i].attributes["msg"]);
-    //   if(typeof vote_freq[vote] != 'undefined') //if(vote_freq[vote] >= 0)
-    //     vote_freq[vote]++;
-    //   else
-    //     vote_freq[vote] = 0;
-    // }
-    //
-    // console.log("vote freq");
-    // console.log(vote_freq);
+      var len = _.keys(this.voteFreq).length;
+      for(var i = 0; i < len; i++) {
+        pieData.push({ value: this.voteFreq[i+1], color: colors[i] });
+      }
 
-    var pieData = [];
-    var colors = ["#F38630", "#E0E4CC", "#69D2E7"];
+      console.log("pie data from view");
+      console.log(pieData);
 
-    var len = _.keys(this.voteFreq).length;
-    for(var i = 0; i < len; i++) {
-      pieData.push({ value: this.voteFreq[i+1], color: colors[i] });
+      var ctx = this.$el.find('#myChart').get(0).getContext("2d");
+      new Chart(ctx).Pie(pieData);
     }
 
-    console.log("pie data from view");
-    console.log(pieData);
-
-    var ctx = this.$el.find('#myChart').get(0).getContext("2d");
-    new Chart(ctx).Pie(pieData);
   }
 
 
