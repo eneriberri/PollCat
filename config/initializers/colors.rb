6baa58daec7be569868633a05540030b75acc65e
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-11-11 11:33:30 -0800

    url share link and edit poll button

--- config/initializers/colors.rb
+++
@@ -1,6 +0,0 @@
-#neon yellow ? , red, cream, bright blue, black, deep bright orange, turquoise bright blue => #69D2E7
-COLORS = ["#C3E86B", "#FF4242",
-          "#E1EDB9", "#00B4FF",
-          "#000000", "#FA6900",
-          "#E0E4CC", "#69D2E7",
-          "#D34017", "#FC354C"]
