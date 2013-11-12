db63c0db2bb22b749e72c2a25539de503a65980d
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-11-12 14:40:03 -0800

    checks for invalid texts and sends replies

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -8,37 +8,48 @@ class VotesController < ApplicationController
     msg = params["Body"][0]
     poll_id = params["Body"][1..-1]
     from_number = params["From"]
-    Vote.create({msg: msg, from: from_number, poll_id: poll_id})
-    votes = Vote.where("poll_id = ?", poll_id)
-    votes.map! { |vote| vote.msg }
 
-    vote_freq = Hash.new(0)
-    votes.each { |vote| vote_freq[vote] += 1 }
+    if txt_valid?(msg, poll_id)
+      Vote.create({msg: msg, from: from_number, poll_id: poll_id})
+      votes = Vote.where("poll_id = ?", poll_id)
+      votes.map! { |vote| vote.msg }
 
-    Pusher['vote_channel'].trigger('new_vote', {
-      message: msg,
-      poll_id: poll_id,
-      votes: vote_freq
-    })
+      vote_freq = Hash.new(0)
+      votes.each { |vote| vote_freq[vote] += 1 }
 
-    send_txt(from_number)
-
-    render :json => "success!"
+      Pusher['vote_channel'].trigger('new_vote', {
+        message: msg,
+        poll_id: poll_id,
+        votes: vote_freq
+      })
+      success_txt = "Howdy! Thanks for voting. -Poll Cat :3"
+      send_txt(from_number, success_txt)
+    else
+      error_txt = "Vote was not received. Please choose from one of the text codes. -Poll Cat D:"
+      send_txt(from_number, error_txt)
+    end
+      render :json => "text received!"
   end
 
-  def send_txt(from_number)
-    number_to_send_to = from_number
+  def txt_valid?(msg, poll_id)
+    poll_ids = Poll.all.map { |poll| poll.id }
+    return false unless poll_ids.include?(poll_id.to_i)
 
+    poll = Poll.find(poll_id)
+    (msg.to_i).between?(1, poll.answers.length)
+  end
+
+  def send_txt(from_number, reply)
+    number_to_send_to = from_number
     twilio_sid = ACCOUNT_SID
     twilio_token = ACCOUNT_TOKEN
     twilio_phone_number = TWILIO_PHONE
 
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
       :to => number_to_send_to,
-      :body => "Howdy! Thanks for voting. -Poll Cat :3"
+      :body => reply
     )
     render :json => "success!"
 
