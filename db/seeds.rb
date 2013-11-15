412a49f9a27820a23fc6013e1c38a60b39655b3c
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-11-15 01:58:41 -0800

    added bg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "GUEST", id: 1, password: "password")
