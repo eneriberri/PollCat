7b8b1ad71b18064d6dd810d04c658102dbaf3b9f
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-11-20 15:30:29 -0800

    swap views

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -6,13 +6,13 @@ PollCatApp.Routers.PollRouter = Backbone.Router.extend({
   routes: {
     "": "index",
     "polls/new": "new",
-    "polls/:id": "show",
-    // "sample_poll": "createSamplePoll"
+    "polls/:id": "show"
   },
 
   index: function() {
     var indexView = new PollCatApp.Views.PollIndex( { collection: PollCatApp.polls } );
-    this.$rootEl.html(indexView.render().$el);
+    // this.$rootEl.html(indexView.render().$el);
+	this._swapView(indexView);
   },
 
   show: function(id) {
@@ -33,54 +33,22 @@ PollCatApp.Routers.PollRouter = Backbone.Router.extend({
                                                     voteFreq: vote_freq,
                                                     id: id } );
 
-    this.$rootEl.html(showView.render().$el);
+    // this.$rootEl.html(showView.render().$el);
+	this._swapView(showView);
   },
 
   new: function() {
     var newView = new PollCatApp.Views.PollNew();
-    this.$rootEl.html(newView.render().$el);
-  }// ,
-//
-//   createSamplePoll: function() {
-//     // var samplePoll = new PollCatApp.Models.Poll({
-//     //   question: "Do you like me?",
-//     //   poll_type: "pie",
-//     //   user_id: 1,
-//     //   urlRoot : '/polls'
-//     // });
-//     // samplePoll.save();
-//
-//     PollCatApp.answers.reset([{body: "Yes", poll_id: 25, count: 0, id: 1}, {body: "You betcha", poll_id: 25, count: 0, id: 2}])
-//     var answers = [PollCatApp.answers.get(1), PollCatApp.answers.get(2)]
-//
-//     PollCatApp.polls.add({
-//       id: 25,
-//       question: "Your couch. Is it mine?",
-//       poll_type: "pie",
-//       user_id: 1
-//     });
-//
-//     PollCatApp.polls.get(25).save();
-//
-//     var samplePoll = PollCatApp.polls.get(25);
-//
-//     var votes = PollCatApp.votes.where({ poll_id: samplePoll.id });
-//
-//     var vote_freq = {};
-//     for(var i = 0; i < votes.length; i++) {
-//       var vote = parseInt(votes[i].attributes["msg"]);
-//       if(typeof vote_freq[vote] != 'undefined')
-//         vote_freq[vote]++;
-//       else
-//         vote_freq[vote] = 1;
-//     }
-//     var showSample = Backbone.Model.extend({urlRoot : '/polls'});
-//
-//     showSample = new PollCatApp.Views.PollShow( { model: samplePoll,
-//                                                   collection: PollCatApp.votes,
-//                                                   voteFreq: vote_freq,
-//                                                   id: samplePoll.id });
-//
-//    this.$rootEl.html(showSample.render().$el);
-//   }
+    // this.$rootEl.html(newView.render().$el);
+	this._swapView(newView);
+  },
+  
+  _swapView: function(newView) {
+	  if(this._prevView) {
+		  this._prevView.remove();
+	  }
+  
+	  this._prevView = newView;
+	  this.$rootEl.html(newView.render().$el);
+  }
 })
